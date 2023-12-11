4d38bd58ddb242d332422df3111dfc5c1cfef4b7
Niklas <derNiklaas@users.noreply.github.com>
2023-12-11 15:33:05 +0100

    day11: remove code duplication

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,32 +1,29 @@
-import kotlin.math.abs
 import utils.AoCDay
 import utils.Vec2D
 
 class Day11 : AoCDay() {
     private val galaxies = input.map { row -> row.map { it == '#' } }
+
     override fun part1(): Any {
-        val stations = expandUniverse2ElectricBoogaloo(getStations(galaxies))
-        var sum = 0
-        for (i in stations.keys) {
-            for (j in i + 1..stations.size) {
-                sum += getDistanceBetween(stations[i]!!, stations[j]!!)
-            }
-        }
-        return sum
+        return solve(1)
     }
 
     override fun part2(): Any {
-        val stations = expandUniverse2ElectricBoogaloo(getStations(galaxies), 999_999)
+        return solve(999_999)
+    }
+
+    private fun solve(shift: Int): Long {
+        val stations = expandUniverse2ElectricBoogaloo(getStations(galaxies), shift)
         var sum = 0L
         for (i in stations.keys) {
             for (j in i + 1..stations.size) {
-                sum += getDistanceBetween(stations[i]!!, stations[j]!!)
+                sum += stations[i]!!.manhattanDistance(stations[j]!!)
             }
         }
         return sum
     }
 
-    private fun expandUniverse2ElectricBoogaloo(stations: Map<Int, Vec2D>, shift: Int = 1): Map<Int, Vec2D> {
+    private fun expandUniverse2ElectricBoogaloo(stations: Map<Int, Vec2D>, shift: Int): Map<Int, Vec2D> {
         var newStations = stations.toMutableMap()
         var yShift = 0L
         galaxies.forEachIndexed { rowIndex, row ->
@@ -64,12 +61,7 @@ class Day11 : AoCDay() {
                 xShift += shift
             }
         }
-        println("max shift x $xShift y $yShift")
         return newStations
-    }
-
-    private fun getDistanceBetween(start: Vec2D, end: Vec2D): Int {
-        return abs(start.x - end.x) + abs(start.y - end.y)
     }
 
     private fun getStations(grid: List<List<Boolean>>): Map<Int, Vec2D> {
