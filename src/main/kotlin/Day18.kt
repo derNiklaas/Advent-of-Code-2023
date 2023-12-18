6a72ab5588d81f66d05519138a7041a365c23849
Niklas <derNiklaas@users.noreply.github.com>
2023-12-18 09:08:38 +0100

    day18: cleanup

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -13,7 +13,6 @@ class Day18 : AoCDay() {
             else -> error("Illegal Direction $dir")
         }
         val hexNumbers = hexCode.removeSurrounding("(#", ")")
-        println(hexNumbers)
         val directionB = when (hexNumbers.last()) {
             '0' -> Vec2D.RIGHT
             '1' -> Vec2D.DOWN
@@ -27,108 +26,25 @@ class Day18 : AoCDay() {
     }
 
     override fun part1(): Any {
+        return calculateArea()
+    }
+
+    override fun part2(): Any {
+        return calculateArea(true)
+    }
+
+    private fun calculateArea(partB: Boolean = false): Long {
         var pos = Vec2D(0, 0)
         var area = 0L
         var borderLength = 0L
         for (instruction in digInstructions) {
-            val newPos = pos + (instruction.direction * instruction.length)
+            val newPos =
+                if (partB) pos + (instruction.directionB * instruction.lengthB) else pos + (instruction.direction * instruction.length)
             area += (newPos.y - pos.y).toLong() * (newPos.x + pos.x).toLong()
-            borderLength += instruction.length
+            borderLength += if (partB) instruction.lengthB else instruction.length
             pos = newPos
         }
         return abs(area) / 2 + borderLength / 2 + 1
-
-
-        /*val border = mutableSetOf<Vec2D>()
-        var currentPos = Vec2D(0, 0)
-        border += currentPos
-        digInstructions.forEach {
-            for (i in 0..<it.length) {
-                currentPos += it.direction
-                border += currentPos
-            }
-        }
-
-        val flooded = flood(border)
-
-        printGrid(border, flooded)
-
-        val minY = border.minBy { it.y }.y
-        val maxY = border.maxBy { it.y }.y
-        val minX = border.minBy { it.x }.x
-        val maxX = border.maxBy { it.x }.x
-
-        return (maxX - minX + 1) * (maxY - minY + 1) - flooded.size */
-    }
-
-    override fun part2(): Any {
-        var x = 0
-        var y = 0
-        var area = 0L
-        var borderLength = 0L
-        for (instruction in digInstructions) {
-            val newX = x + (instruction.directionB * instruction.lengthB).x
-            val newY = y + (instruction.directionB * instruction.lengthB).y
-            area += (newY - y).toLong() * (newX + x).toLong()
-            borderLength += instruction.lengthB
-            x = newX
-            y = newY
-        }
-        return abs(area) / 2 + borderLength / 2 + 1
-    }
-
-    private fun printGrid(border: Set<Vec2D>, flooded: Set<Vec2D>) {
-        val minY = border.minBy { it.y }.y
-        val maxY = border.maxBy { it.y }.y
-        val minX = border.minBy { it.x }.x
-        val maxX = border.maxBy { it.x }.x
-
-        for (y in minY..maxY) {
-            for (x in minX..maxX) {
-                if (Vec2D(x, y) in border) print("#")
-                else if (Vec2D(x, y) in flooded) print('F')
-                else print(".")
-            }
-            println()
-        }
-    }
-
-    private fun flood(border: Set<Vec2D>): Set<Vec2D> {
-        val flooded = mutableSetOf<Vec2D>()
-        val minY = border.minBy { it.y }.y
-        val maxY = border.maxBy { it.y }.y
-        val minX = border.minBy { it.x }.x
-        val maxX = border.maxBy { it.x }.x
-
-        val floodQueue = mutableSetOf<Vec2D>()
-
-        for (y in minY..maxY) {
-            val left = Vec2D(minX, y)
-            val right = Vec2D(maxX, y)
-            if (left !in border) floodQueue += left
-            if (right !in border) floodQueue += right
-        }
-        for (x in minX..maxX) {
-            val top = Vec2D(x, minY)
-            val bottom = Vec2D(x, maxY)
-            if (top !in border) floodQueue += top
-            if (bottom !in border) floodQueue += bottom
-        }
-
-        while (floodQueue.isNotEmpty()) {
-            val pos = floodQueue.first()
-            floodQueue.remove(pos)
-
-            if (pos in flooded) continue
-            if (pos in border) continue
-            flooded += pos
-            pos.getNeighbours().forEach {
-                if (it.x in minX..maxX && it.y in minY..maxY) {
-                    floodQueue += it
-                }
-            }
-        }
-        return flooded
     }
 }
 
